a76100737845406c35c77b634ed1637a71dab922
veereshn696-alt <veereshn696@gmail.com>
2025-11-20 17:33:03 +0530

    Update ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -1,18 +1,19 @@
 import sys
-if len(sys.argv) !=3:
-  print("usage : python student.py <name> <salary> <empid>")
-  sys.exit(1)
-  script_name=sys.argv[0]
-  name=sys.argv[1]
-  salary=sys.argv[2]
-  empid=sys.argv[3]
-else :
-  script_name=sys.argv[0]
-  name="veeresh"
-  salary="5000"
-  empid="178"
-  
-print("script name : ",script_name)
-print("employee name  : ",name)
-print("salary : ",salary)
-print("employee id : ",empid)
+
+# Expecting: python student.py <name> <salary> <empid>
+if len(sys.argv) == 4:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    salary = sys.argv[2]
+    empid = sys.argv[3]
+else:
+    print("Invalid arguments. Using default values...")
+    script_name = sys.argv[0]
+    name = "veeresh"
+    salary = "5000"
+    empid = "178"
+
+print("script name :", script_name)
+print("employee name :", name)
+print("salary :", salary)
+print("employee id :", empid)
