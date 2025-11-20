6c4f69aaa12d271dfa86fc97a91cc5aee253bca9
veereshn696-alt <veereshn696@gmail.com>
2025-11-20 17:26:15 +0530

    Update ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -1,6 +1,6 @@
 import sys
 if len(sys.argv) !=3:
-  print("usage : python student.py <name> <rollno>")
+  print("usage : python student.py <name> <salary> <empid>")
   sys.exit(1)
   script_name=sys.argv[0]
   name=sys.argv[1]
